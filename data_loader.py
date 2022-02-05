40d55bf34f9fa3a3f24861537330848735eb4e51
Jonator3 <joey.pehlke@gmail.com>
2022-02-05 16:12:07 +0100

    correct df(t)

--- data_loader.py
+++ data_loader.py
@@ -13,17 +13,28 @@ def get_file_token(file_path):
 
 front_corpus_dict = {}
 back_corpus = []
+doc_freq = {}
+doc_count = 0
 
 
 def load():
-    global front_corpus_dict, back_corpus
+    global front_corpus_dict, back_corpus, doc_count, doc_freq
     front_corpus_dict = {}
     back_corpus = []
+    doc_freq = {}
+    doc_count = 0
     for i in range(15):
         print("loading cluster", i)
         token = []
         for file in os.listdir("clusters/"+str(i)):
-            token += get_file_token("clusters/"+str(i)+"/"+file)
+            sub_token = get_file_token("clusters/"+str(i)+"/"+file)
+            doc_count += 1
+            token += sub_token
+            for t in set(sub_token):
+                if doc_freq.get(t) is None:
+                    doc_freq[t] = 1
+                else:
+                    doc_freq[t] += 1
         front_corpus_dict[i] = token
         back_corpus += token
     print("loading done")
--- main.py
+++ main.py
@@ -6,30 +6,28 @@ from nltk.probability import FreqDist
 import data_loader as data
 
 
+def inverse_doc_frequency(term: str, total_num_docs=data.doc_count):
+    return total_num_docs / data.doc_freq.get(term)
 
 
-# TODO: might be false
-def term_frequency(doc_freq_dist: FreqDist, term: str):
-    return doc_freq_dist.freq(term)# / doc_freq_dist.freq(doc_freq_dist.max())
-
-
-def inverse_doc_frequency(doc_freq, total_num_docs=15):
-    return total_num_docs / doc_freq
-
-
-def tf_idf(term: str, forground: List[str], background: List[str]):
+def tf_idf(term: str, forground: List[str]):
     doc_freq_dist = nltk.FreqDist(forground)
-    return doc_freq_dist.freq(term) * math.log2(inverse_doc_frequency(background.count(term)+1))
+    return doc_freq_dist.freq(term) * math.log2(inverse_doc_frequency(term))
 
 # tf-idf(t, d) := tf(t, d) * idf(t)
 # idf(t) := (N/(df(t))
-# df(t) := anzahl von t inm document
+# df(t) := anzahl von documenten mit token t
 # tf(t, d) := frequency of t in d = doc_freq_dist.freq(term)
 
-print(tf_idf("Venus", data.front_corpus_dict.get(0), data.back_corpus))
-print(tf_idf("Mars", data.front_corpus_dict.get(0), data.back_corpus))
-print(tf_idf("the", data.front_corpus_dict.get(0), data.back_corpus))
 
-print(tf_idf("Venus", data.front_corpus_dict.get(1), data.back_corpus))
-print(tf_idf("Mars", data.front_corpus_dict.get(1), data.back_corpus))
-print(tf_idf("the", data.front_corpus_dict.get(1), data.back_corpus))
+output = open("tf_idf_output.csv", "w")
+output.write("term,total_count,tf_idf(0),tf_idf(1),tf_idf(2),tf_idf(3),tf_idf(4),tf_idf(5),tf_idf(6),tf_idf(7),tf_idf(8),tf_idf(9),tf_idf(10),tf_idf(11),tf_idf(12),tf_idf(13),tf_idf(14)\n")
+
+term_count = len(set(data.back_corpus))
+for i, term in enumerate(set(data.back_corpus)): #  every word used in at least on of the documents
+    print(i, "/", term_count, ":", term)
+    count = data.back_corpus.count(term)
+    output.write(term+","+str(count))
+    for fi in data.front_corpus_dict.keys():
+        output.write("," + str(round(tf_idf(term, data.front_corpus_dict.get(fi))*1000000, 1)))
+    output.write("\n")
