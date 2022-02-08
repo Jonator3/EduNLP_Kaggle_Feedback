9ecb91bc463c54bc64f333a69c8ae32af6ed380d
Jonator3 <joey.pehlke@gmail.com>
2022-02-08 12:54:28 +0100

    correct get token count + code optimization

--- data_loader.py
+++ data_loader.py
@@ -15,15 +15,21 @@ front_corpus_dict = {}
 back_corpus = []
 doc_freq = {}
 doc_count = 0
+token_count_dict = {}
+front_corpus_freqdist_dict = {}
 
+def load(clusters=15):
+    global front_corpus_dict, back_corpus, doc_count, doc_freq, token_count_dict, front_corpus_freqdist_dict
 
-def load():
-    global front_corpus_dict, back_corpus, doc_count, doc_freq
+    # reset all values
     front_corpus_dict = {}
+    front_corpus_freqdist_dict = {}
     back_corpus = []
     doc_freq = {}
+    token_count_dict = {}
     doc_count = 0
-    for i in range(15):
+
+    for i in range(clusters):
         print("loading cluster", i)
         token = []
         for file in os.listdir("clusters/"+str(i)):
@@ -37,8 +43,17 @@ def load():
                     doc_freq[t] += 1
         front_corpus_dict[i] = token
         back_corpus += token
+        front_corpus_freqdist_dict[i] = nltk.FreqDist(token)
+    print("counting tokens")
+    bcs = back_corpus.copy()
+    bcs.sort()
+    current_term = None
+    segment_start = -1
+    for i, term in enumerate(bcs):
+        if term != current_term:
+            if current_term is not None:
+                token_count_dict[current_term] = i - segment_start
+            segment_start = i
+            current_term = term
     print("loading done")
-
-
-load()
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import math
 from typing import List
 
@@ -10,9 +11,8 @@ def inverse_doc_frequency(term: str, total_num_docs=data.doc_count):
     return total_num_docs / data.doc_freq.get(term)
 
 
-def tf_idf(term: str, forground: List[str]):
-    doc_freq_dist = nltk.FreqDist(forground)
-    return doc_freq_dist.freq(term) * math.log2(inverse_doc_frequency(term))
+def tf_idf(term: str, forground_freqdist: List[str]):
+    return forground_freqdist.freq(term) * math.log2(inverse_doc_frequency(term))
 
 # tf-idf(t, d) := tf(t, d) * idf(t)
 # idf(t) := (N/(df(t))
@@ -20,14 +20,41 @@ def tf_idf(term: str, forground: List[str]):
 # tf(t, d) := frequency of t in d = doc_freq_dist.freq(term)
 
 
-output = open("tf_idf_output.csv", "w")
-output.write("term,total_count,tf_idf(0),tf_idf(1),tf_idf(2),tf_idf(3),tf_idf(4),tf_idf(5),tf_idf(6),tf_idf(7),tf_idf(8),tf_idf(9),tf_idf(10),tf_idf(11),tf_idf(12),tf_idf(13),tf_idf(14)\n")
+def calc_all_tfidf(min_count=5):
+    output = open("tf_idf_output.csv", "w")
+    output.write("term,total_count,tf_idf(0),tf_idf(1),tf_idf(2),tf_idf(3),tf_idf(4),tf_idf(5),tf_idf(6),tf_idf(7),tf_idf(8),tf_idf(9),tf_idf(10),tf_idf(11),tf_idf(12),tf_idf(13),tf_idf(14)\n")
 
-term_count = len(set(data.back_corpus))
-for i, term in enumerate(set(data.back_corpus)): #  every word used in at least on of the documents
-    print(i, "/", term_count, ":", term)
-    count = data.back_corpus.count(term)
-    output.write(term+","+str(count))
-    for fi in data.front_corpus_dict.keys():
-        output.write("," + str(round(tf_idf(term, data.front_corpus_dict.get(fi))*1000000, 1)))
-    output.write("\n")
+    term_count = len(set(data.back_corpus))
+    for i, term in enumerate(set(data.back_corpus)):  # every word used in at least on of the documents
+        print(i, "/", term_count, ":", term)
+        count = data.token_count_dict.get(term)
+        output.write(term + "," + str(count))
+        for fi in data.front_corpus_dict.keys():
+            output.write("," + str(round(tf_idf(term, data.front_corpus_freqdist_dict.get(fi)), 7)))
+        output.write("\n")
+
+
+def calc_count_dist():
+    count_dist = {}
+    for term in set(data.back_corpus):
+        count = data.token_count_dict.get(term)
+        if count_dist.get(count) is None:
+            count_dist[count] = 1
+        else:
+            count_dist[count] += 1
+    output = open("token_count_distribution_output.txt", "w")
+
+    for i in range(1000):
+        count = count_dist.get(i+1)
+        if count is None:
+            count = 0
+        print(i+1, ":", count)
+        output.write(str(count) + "\n")
+
+
+start_time = datetime.datetime.now()
+data.load()
+
+calc_all_tfidf()
+
+print("\nTime:", datetime.datetime.now() - start_time)
